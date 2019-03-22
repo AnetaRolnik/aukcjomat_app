59b65d71a6fbc956060a68b52d3fb9154c8433bf
AnetaRolnik <aneta.r@poczta.onet.eu>
2019-03-22 23:18:02 +0100

    Project

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+const gulp            = require("gulp");
+const browserSync     = require("browser-sync").create();
+const sass            = require("gulp-sass");
+const watch           = require("gulp-watch");
+const autoprefixer    = require("gulp-autoprefixer");
+const sourcemaps      = require("gulp-sourcemaps"); 
+const plumber         = require("gulp-plumber"); 
+const rename          = require("gulp-rename"); 
+const colors          = require("ansi-colors");
+const notifier        = require("node-notifier");
+const wait            = require('gulp-wait');
+const csso            = require('gulp-csso');
+
+
+function showError(err) {
+    notifier.notify({
+        title: 'Error in sass',
+        message: err.messageFormatted
+      });
+
+    console.log(colors.red('==============================='));
+    console.log(colors.red(err.messageFormatted));
+    console.log(colors.red('==============================='));
+    this.emit('end');
+}
+
+gulp.task("browseSync", function() {
+    browserSync.init({
+        server: "./",
+        notify: true,
+        host: "192.168.0.24",
+        open: true,
+    });
+});
+
+gulp.task("sass", function() {
+    return gulp.src("src/scss/style.scss")
+        .pipe(wait(500))
+        .pipe(plumber({ 
+            errorHandler: showError
+        }))
+        .pipe(sourcemaps.init()) 
+        .pipe(sass({
+            outputStyle: "compressed"
+        }))
+        .pipe(autoprefixer({
+            browsers: ["> 1%"]
+        })) 
+        .pipe(csso()) 
+        .pipe(rename({
+            suffix: ".min",
+            basename: "style"
+        }))
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("dist/css"))
+        .pipe(browserSync.stream({match: "**/*.css"}));
+});
+
+gulp.task("watch", function() {
+    gulp.watch("src/scss/**/*.scss", ["sass"]);
+    gulp.watch("*.html").on("change", browserSync.reload);
+});
+
+gulp.task("default", function() {
+    console.log(colors.yellow("======================= start ======================="));
+    gulp.start(["sass", "browseSync", "watch"]);
+});
